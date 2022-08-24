0ed15649fdf13546b07a18f0cdbd3dc8716d025c
Anton Shiryaev <med_phisiker@mail.ru>
2022-08-25 01:00:46 +1000

    added support for multiple mount volumes

--- docker/run.sh
+++ docker/run.sh
@@ -167,6 +167,7 @@ sudo xhost +si:localuser:root
 
 sudo docker run --runtime nvidia -it --rm \
 		--network host \
+                --privileged \
 		-e DISPLAY=$DISPLAY \
 		-v /tmp/.X11-unix/:/tmp/.X11-unix \
 		-v /tmp/argus_socket:/tmp/argus_socket \
